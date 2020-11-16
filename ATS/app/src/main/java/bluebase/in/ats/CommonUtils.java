543648ea426051975a9b2c0a900e06399159ae58
Mohana Krishnan S <mohanakrishnan.s@bluebase.in>
2020-11-16 09:29:15 +0530

    new fragments

---
+++ ATS/app/src/main/java/bluebase/in/ats/CommonUtils.java
@@ -0,0 +1,5 @@
+package bluebase.in.ats;
+
+public class CommonUtils {
+    static String IP = "http://10.0.2.2";
+}
--- ATS/app/src/main/java/bluebase/in/ats/MainActivity.java
+++ ATS/app/src/main/java/bluebase/in/ats/MainActivity.java
@@ -1,14 +1,145 @@
 package bluebase.in.ats;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentManager;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    Context context = this;
+    String email;
+    private DrawerLayout drawer;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = getIntent();
+        email = intent.getStringExtra("email");
+
+        Toolbar toolBar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolBar);
+
+        drawer = findViewById(R.id.patient_drawer_layout);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        View headerView = navigationView.getHeaderView(0);
+        navigationView.setNavigationItemSelectedListener(this);
+        TextView emailTextView = headerView.findViewById(R.id.email);
+        emailTextView.setText(email);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolBar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        ImageView backButton = headerView.findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawer.closeDrawer(GravityCompat.START);
+            }
+        });
+
+        if(savedInstanceState == null) {
+            getSupportFragmentManager().popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE);
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .addToBackStack("homeFragment")
+                    .replace(R.id.fragment_container, new HomeFragment(), "homeFragment")
+                    .commit();
+            navigationView.setCheckedItem(R.id.nav_home);
+        }
+
     }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch(menuItem.getItemId()){
+            case R.id.nav_home:
+                getSupportFragmentManager().popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE);
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .addToBackStack("homeFragment")
+                        .replace(R.id.fragment_container, new HomeFragment(), "homeFragment")
+                        .commit();
+                break;
+
+            case R.id.nav_profile:
+                getSupportFragmentManager().popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE);
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .addToBackStack("profileFragment")
+                        .replace(R.id.fragment_container, new ProfileFragment(), "profileFragment")
+                        .commit();
+                break;
+
+            case R.id.nav_task_creation:
+                getSupportFragmentManager().popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE);
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .addToBackStack("taskCreationFragment")
+                        .replace(R.id.fragment_container, new TaskCreationFragment(), "taskCreationFragment")
+                        .commit();
+                break;
+
+            case R.id.nav_report:
+                getSupportFragmentManager().popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE);
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .addToBackStack("reportFragment")
+                        .replace(R.id.fragment_container, new ReportFragment(), "reportFragment")
+                        .commit();
+                break;
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+
+        return true;
+    }
+
+    @Override
+    public void onBackPressed(){
+        if(drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        }else{
+            int fragmentsInStack = getSupportFragmentManager().getBackStackEntryCount();
+
+            if (fragmentsInStack > 1) {
+                getSupportFragmentManager().popBackStack();
+            }else {
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
+                alertDialogBuilder.setCancelable(false);
+                alertDialogBuilder.setTitle("ATS");
+                alertDialogBuilder.setMessage("Do you want to logout?");
+                alertDialogBuilder.setPositiveButton(android.R.string.ok,
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                                startActivity(intent);
+                            }
+                        });
+
+                final AlertDialog alertDialog = alertDialogBuilder.create();
+                alertDialog.show();
+            }
+        }
+    }
+
 }
